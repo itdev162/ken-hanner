2b61995295a9773500c49e32f132ba6fbf03c55f
ken <hannerk@gmatc.matc.edu>
2021-10-05 13:35:33 -0500

    prep for Activity 5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ class App extends React.Component {
       })
     })
       .catch((error) => {
-        console.error('Error fetching data: ${error}');
+        console.error(`Error fetching data: ${error}`);
       })
   }
 
