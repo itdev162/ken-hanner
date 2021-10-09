362962477b3bdd098c8cea4522490ce76bfa50de
ken <hannerk@gmatc.matc.edu>
2021-10-09 14:48:59 -0500

    add pizzazz to test post language

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -10,13 +10,13 @@ namespace Persistence
             if (context.Posts.Count() == 0) {
                 List < Post > seedPosts = new List < Post > {
                     new Post() {
-                        Title = "First Post", Body = "This is the body of my first post. It will be used if there are no posts in the database."
+                        Title = "Test Post 1", Body = "I must not test post. Test post is the mind-killer. Test post is the little-death that brings total obliteration."
+                    },                                 
+                    new Post() {
+                        Title = "Test Post 2", Body = "I will face my test post. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path."
                     },
                     new Post() {
-                        Title = "Second Post", Body = "This is the body of my second post. It will be used if there are no posts in the database."
-                    },
-                    new Post() {
-                        Title = "Third Post", Body = "This is the body of my third post. It will be used if there are no posts in the database."
+                        Title = "Test Post 3", Body = "Where the test post has gone there will be nothing. Only I will remain."
                     }
                 };
 
